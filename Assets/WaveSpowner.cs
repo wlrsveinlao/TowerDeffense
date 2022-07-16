7cfc591abd03942cd94d69eeaaf6b27d29f84f11
Danil "wlrsveinlao" Matveev <jolynaded@gmail.com>
2022-07-16 15:25:49 +0300

    description
    add description

--- Assets/WaveSpowner.cs
+++ Assets/WaveSpowner.cs
@@ -4,40 +4,56 @@ using UnityEngine.UI;
 
 public class WaveSpowner : MonoBehaviour
 {
-    public Transform enemyPrefab;
+    // ref to own prefabs things, like enemys and "start" object
+    public Transform enemyPrefab;   
     public Transform SpownPoint;
 
+    // time between waves, defoult 5sec
     public float TimeBetWaves = 5f;
 
+    // ref to Convas Text, for show time between waves
     public Text countDownText;
 
+    // first wave countdown
     private float countdown = 2f;
+    
+    //count of waves;
     private int waveIndex = 0;
 
 
     private void Update()
-    {
+    {   
+        //if countdown will == 0sec, then will spown first wave
         if (countdown <= 0f)
         {
+            // after first one, countdown will enter the "timeBetWaves" count
             StartCoroutine(SpawnWave());
             countdown = TimeBetWaves;
         }
+        // is this need for fps 
         countdown -= Time.deltaTime;
-
-        countDownText.text = Mathf.Round(countdown).ToString();
+        // remove float thing of nubers to int, and return sec to main camera
+        countDownText.text = Mathf.Round(countdown).ToString() + " sec";
     }
+    // using Ienumerator to make routine, and fix copy enemyes
     IEnumerator SpawnWave()
     {
+        // just count of waves 
         waveIndex++;
+
+        // on every wave, enemy will creates +1 like wavecount
         for (int i = 0; i < waveIndex; i++)
         {
             SpawnEnemy();
+
+            // is that distance of enemy, but of spawnTime;
             yield return new WaitForSeconds(0.5f);
         }
         
     }
     void SpawnEnemy()
     {
+        //Instantiate is metod for Spawn something in game, when we are playing
         Instantiate(enemyPrefab, SpownPoint.position, SpownPoint.rotation);
 
     }
